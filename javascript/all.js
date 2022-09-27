e3eab1c89a62252e67c2d4b86eb08faa50eb5a1a
Freddie <lin.pengyu@outlook.com>
2022-09-27 22:41:53 +0800

    adds form validation

--- javascript/all.js
+++ javascript/all.js
@@ -1,10 +1,23 @@
-$(document).ready(function () {
-  $(".dog1").hover(
-    function () {
-      $(this).attr("src", "/images/dog01-hover-1.png");
-    },
-    function () {
-      $(this).attr("src", "/images/dog06.png");
-    }
-  );
-});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
